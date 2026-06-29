0b7e67a5b6b5d169d1254294735654489c1b5aa9
Roushankumar Mishra <mishraroushan455@gmail.com>
2026-06-29 16:53:05 +0530

    feat: implement authentication middleware and JWT verification, add user type to Express request

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,15 @@
 import jwt from "jsonwebtoken";
 
-interface JwtPayload {
+export interface JwtPayload {
   userId: number;
   email: string;
 }
+
+/*
+===================================
+GENERATE TOKEN
+===================================
+*/
 
 export const generateToken = (payload: JwtPayload): string => {
   const secret = process.env.JWT_SECRET;
@@ -15,4 +21,26 @@ export const generateToken = (payload: JwtPayload): string => {
   return jwt.sign(payload, secret, {
     expiresIn: "1h",
   });
+};
+
+/*
+===================================
+VERIFY TOKEN
+===================================
+*/
+
+export const verifyToken = (token: string): JwtPayload => {
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret) {
+    throw new Error("JWT_SECRET is missing");
+  }
+
+  const decoded = jwt.verify(token, secret);
+
+  if (typeof decoded === "string") {
+    throw new Error("Invalid token");
+  }
+
+  return decoded as JwtPayload;
 };
