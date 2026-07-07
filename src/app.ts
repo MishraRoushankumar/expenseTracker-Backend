7929d73b4d3d36d452fae050d31ed2147f76f351
Roushankumar Mishra <mishraroushan455@gmail.com>
2026-07-07 23:21:06 +0530

    feat: add structured logging infrastructure

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import express from "express";
+import cors from "cors";
+import { httpLogger } from "./logger/httpLogger.js";
 import routes from "./routes/index.js";
-import { loggerMiddleware } from "./middlewares/logger.middleware.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use(express.json());
-
-app.use(loggerMiddleware);
-
-app.use(routes);
-
+app.use(cors());
+app.use(express.json());
+app.use(httpLogger);
+app.use("/api/v1", routes);
 app.use(errorMiddleware);
 
 export default app;
